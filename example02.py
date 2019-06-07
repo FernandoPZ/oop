8dc31bbad51b4ac5ea10b781d7c07b12fb6b37a1
FernandoPZ <50506780+FernandoPZ@users.noreply.github.com>
2019-06-07 12:54:55 -0500

    upload example for

--- example02.py
+++ example02.py
@@ -1,2 +1,14 @@
-for i in range(10):
+for i in range(5):
  print ("Hello",i)
+
+numbers = ['one','two','three']
+for i in numbers:
+ print ("Hello",i)
+
+numbers = [10.3,14,13]
+for i in numbers:
+ print ("Hello",i)
+
+numbers = [10.3,14,13]
+for i in numbers:
+ print ("Hello",(i+1))
