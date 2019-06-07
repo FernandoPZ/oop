8accd6998088887bc23d97fd9a0c506356e20974
FernandoPZ <50506780+FernandoPZ@users.noreply.github.com>
2019-06-07 12:05:37 -0500

    Example of python (errors)

---
+++ example01.py
@@ -0,0 +1,10 @@
+number_01 = 10.5
+number_02 = 0
+add = 0
+if number_01 < number_02:
+ add = number_01 + number_02
+ print ("Add" , add)
+else:
+ add = number_01 / number_02
+ print ("Add" , add)
+print ("End")
